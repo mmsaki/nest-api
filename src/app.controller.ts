18ea5844222eea018d674ed2896c14507b9bb4b5
Meek <meek10x@gmail.com>
2023-03-06 20:33:32 -0600

    added swagger api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/other_thing')
+  getOtherThing(): string {
+    return 'Wrong thing';
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  counter = 0;
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' + this.counter++;
   }
 }
